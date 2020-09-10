b34f8320144fa6fa96325277a1f7a533481d68f0
Daniel Soares <daniel.soares@me.com>
2020-09-10 16:30:24 -0300

    :sparkles: query logo

--- content/settings/styles.md
+++ content/settings/styles.md
@@ -1,8 +1,9 @@
 ---
+key: styles
 borderRadius: '1rem'
 buttonsColor: ' #00d1b7'
 fontColor: '#262626'
 footerFontColor: '#fff'
 background: '#262626'
-logo: 'logo.svg'
+logo: 'logo.png'
 ---
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import Img from 'gatsby-image'
+import { useStaticQuery, graphql } from 'gatsby'
 
-const Header = () => (
-  <>
-    <h1>Hello world</h1>
-  </>
-)
+import * as S from './styled'
+
+const Header = () => {
+  const { markdownRemark } = useStaticQuery(
+    graphql`
+      query {
+        markdownRemark(frontmatter: { key: { eq: "styles" } }) {
+          frontmatter {
+            logo {
+              childImageSharp {
+                fluid(maxWidth: 256, quality: 100) {
+                  ...GatsbyImageSharpFluid_withWebp
+                }
+              }
+            }
+          }
+        }
+      }
+    `
+  )
+
+  return (
+    <S.Header>
+      <Img fluid={markdownRemark.frontmatter.logo.childImageSharp.fluid} />
+    </S.Header>
+  )
+}
 
 export default Header
