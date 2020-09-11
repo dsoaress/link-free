0f0111960e2343e338be391d5fe90fda98d2d0d4
Daniel Soares <daniel.soares@me.com>
2020-09-11 14:43:39 -0300

    :mag: fix title

--- src/components/SEO/index.js
+++ src/components/SEO/index.js
@@ -24,8 +24,7 @@ function SEO({ description, lang, meta, title }) {
       htmlAttributes={{
         lang
       }}
-      title={title}
-      titleTemplate={`%s | ${site.siteMetadata.title}`}
+      title={site.siteMetadata.title}
       meta={[
         {
           name: `description`,
@@ -70,7 +69,7 @@ SEO.propTypes = {
   description: PropTypes.string,
   lang: PropTypes.string,
   meta: PropTypes.arrayOf(PropTypes.object),
-  title: PropTypes.string.isRequired
+  title: PropTypes.string
 }
 
 export default SEO
