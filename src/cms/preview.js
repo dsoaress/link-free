86ef643666bd17585b8ebc5e728eb7c31ebfa6cd
Daniel Soares <daniel.soares@me.com>
2020-09-19 16:35:53 -0300

    :sparkles: adds preview to Netlify CMS

--- src/cms/preview.js
+++ src/cms/preview.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import Layout from '../components/Layout'
 
-const Preview = ({ entry, getAsset }) => (
-  <Layout
-    background={entry.getIn(['data', 'background'])}
-    logo={getAsset(entry.getIn(['data', 'logo']))}
-    fontColor={entry.getIn(['data', 'fontColor'])}
-    borderRadius={entry.getIn(['data', 'borderRadius'])}
-    buttonsColor={entry.getIn(['data', 'buttonsColor'])}
-    footerFontColor={entry.getIn(['data', 'footerFontColor'])}
-  ></Layout>
-)
+const Preview = ({ entry, getAsset, widgetFor }) => {
+  console.log(widgetFor('links'))
+  const links = entry.getIn(['data', 'links'])
+  return (
+    <Layout
+      background={entry.getIn(['data', 'background'])}
+      logo={getAsset(entry.getIn(['data', 'logo']))}
+      links={links.toJS()}
+      fontColor={entry.getIn(['data', 'fontColor'])}
+      borderRadius={entry.getIn(['data', 'borderRadius'])}
+      buttonsColor={entry.getIn(['data', 'buttonsColor'])}
+      footerFontColor={entry.getIn(['data', 'footerFontColor'])}
+    ></Layout>
+  )
+}
 
 export default Preview
