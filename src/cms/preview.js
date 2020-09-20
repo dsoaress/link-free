cd9f052a9a07514ea0a5ecddd4a23de9e1cf4c73
Daniel Soares <daniel.soares@me.com>
2020-09-19 22:59:23 -0300

    :bug: fix CMS preview

--- src/cms/preview.js
+++ src/cms/preview.js
@@ -1,19 +1,23 @@
 import React from 'react'
-import IndexPageTemplate from '../templates/IndexPageTemplate'
+
+import Layout from '../components/Layout'
+import Header from '../components/Header'
+import Links from '../components/Links'
+import Footer from '../components/Footer'
 
 const Preview = ({ entry, getAsset, widgetFor }) => {
-  console.log(widgetFor('links'))
   const links = entry.getIn(['data', 'links'])
   return (
-    <IndexPageTemplate
-      background={entry.getIn(['data', 'background'])}
-      logo={getAsset(entry.getIn(['data', 'logo']))}
-      links={links.toJS()}
-      fontColor={entry.getIn(['data', 'fontColor'])}
-      borderRadius={entry.getIn(['data', 'borderRadius'])}
-      buttonsColor={entry.getIn(['data', 'buttonsColor'])}
-      footerFontColor={entry.getIn(['data', 'footerFontColor'])}
-    />
+    <Layout background={entry.getIn(['data', 'background'])}>
+      <Header logo={getAsset(entry.getIn(['data', 'logo']))} />
+      <Links
+        links={links.toJS()}
+        fontColor={entry.getIn(['data', 'fontColor'])}
+        borderRadius={entry.getIn(['data', 'borderRadius'])}
+        buttonsColor={entry.getIn(['data', 'buttonsColor'])}
+      />
+      <Footer footerFontColor={entry.getIn(['data', 'footerFontColor'])} />
+    </Layout>
   )
 }
 
