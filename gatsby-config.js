8a0cadfa2af1876ad8d4d6e30e155f695431c53c
Daniel Soares <daniel.soares@me.com>
2020-12-15 11:32:59 -0300

    refactor: ♻️ Simplifies gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,1 @@
-const fs = require('fs')
-const yaml = require('js-yaml')
-const metadata = yaml.safeLoad(
-  fs.readFileSync('./content/metadata.yaml', 'utf8')
-)
-
-module.exports = {
-  plugins: [
-    {
-      resolve: `@marscollective/gatsby-theme-link-free`,
-      options: {
-        metadata
-      }
-    }
-  ]
-}
+module.exports = { plugins: [`@marscollective/gatsby-theme-link-free`] }
