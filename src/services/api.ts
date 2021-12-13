aaaaedb73c593de6ff9f9b29c453f6f7a05cf4b1
Daniel Soares <daniel.soares@me.com>
2021-12-13 16:04:31 -0300

    fix: api path and sharp

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,5 @@
 import axios from 'axios'
 
-const { RAILWAY_STATIC_URL } = process.env
-
 export const api = axios.create({
-  baseURL: `${RAILWAY_STATIC_URL}/api` || 'http://localhost:3000/api'
+  baseURL: '/api'
 })
